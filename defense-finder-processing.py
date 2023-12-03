fcfaf9b2b1bbd678b0ae92814442c49989959555
GM110Z <72254136+GM110Z@users.noreply.github.com>
2023-12-03 16:03:56 +0000

    Update defense-finder-processing.py

--- defense-finder-processing.py
+++ defense-finder-processing.py
@@ -1,15 +1,1 @@
-#!/usr/bin/env python
-# coding: utf-8
-import pandas as pd
-#get header name into list 
-hd = pd.read_csv('python-header.tsv', sep=".", low_memory=False)
-s=list(hd)
-#process defense finder output
-file_name = "defense_finder_systems.tsv"
-df = pd.read_csv(file_name, sep="\t", low_memory=False)
-df2 = df[["subtype",'protein_in_syst']]
-i = df2.columns.get_loc('protein_in_syst')
-df3 =df2.protein_in_syst.str.split(",", expand = True)     
-df4=pd.concat([df2.iloc[:, :i], df3, df2.iloc[:, i+1:]], axis=1)
-df4['genome']=s[0]
-df4.to_csv(s[0]+"_"+'defense_finder.csv', '\t', header=True)
+
